e899d0b73e453e2d409563d64b04b4261981676e
theoL <theo1007@outlook.fr>
2019-03-13 15:08:56 +0100

    Ajout de l'exo 4

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+  $nomDuVisiteur = "John";
+  $ageDuVisiteur = "54";
+  $poids = 75.4;
+  $JeSuisUnZero = true;
+  $JeSuisUnBonEnPhp = false;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+        echo $nomDuVisiteur;
+       ?>
+    </p>
+    <p>
+      <?php
+        echo $ageDuVisiteur;
+       ?>
+    </p>
+    <p>
+      <?php
+        echo $poids;
+       ?>
+    </p>
+    <p>
+      <?php
+        echo $JeSuisUnZero;
+       ?>
+    </p>
+  </body>
+</html>
